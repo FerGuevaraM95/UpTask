0ab94b8a6f64bd3541c07b28239bc5b3326c9ac4
Fernando Guevara <luisfernandoguevara95@gmail.com>
2019-07-08 22:42:09 -0500

    (Add):Almacenando usuarios en la DB

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,5 +1,20 @@
+const Usuarios = require('../models/Usuarios');
+
 exports.formCrearCuenta = (req, res) => {
     res.render('crearCuenta', {
         nombrePagina: 'Crear cuenta en Uptask'
     });
+}
+
+exports.crearCuenta = (req, res) => {
+    // leer los datos
+    const {email, password} = req.body;
+    // crear el usuario
+    Usuarios.create({
+        email,
+        password
+    })
+    .then(() => {
+        res.redirect('/iniciar-sesion')
+    })
 }
